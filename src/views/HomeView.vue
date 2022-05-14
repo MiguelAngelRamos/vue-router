24b37b7a21768d1cc3f36909d295a87aaf9fb78d
Miguel Ramos <return.infox@gmail.com>
2022-05-14 17:38:50 -0400

    Segundo commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,26 @@
     <div class="row" v-for="post of posts" :key="post.id">
       <div class="col">
         <h3>{{ post.title.charAt(0).toUpperCase()+ post.title.slice(1) }}</h3>
+        <!-- Autor -->
+        <router-link :to="`/user/${post.userId}`">Autor</router-link>
         <p>{{ post.body}}</p>
+
+        <button 
+          class="mb-2 btn btn-secondary"
+          @click="comentariosId(post.id)"
+        > Ver comentarios
+        </button>
+
+        <template v-for="cFiltrado of comentariosFiltrados" :key="cFiltrado.id">
+          <div class="mt-3 col-sm-10 offset-sm-1" v-if="cFiltrado.postId === post.id">
+            <p class="mt-4 fw-bold">User: {{ cFiltrado.email}}</p>
+            <br>
+            <p>Respuesta: </p>
+            <p>{{ cFiltrado.body}}</p>
+          </div>
+        </template>
       </div>
+      <hr/>
     </div>
   </div>
 </template>
@@ -22,24 +40,45 @@ export default {
     }
   },
   methods: {
+    comentariosId(id) {
+      console.log(id); // id del posts
+      this.comentariosFiltrados = this.comentarios.filter( comentario => comentario.postId == id);
+      console.log(this.comentariosFiltrados);
+    },
     async consumirPosts() {
       try {
         const data = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-        const getPosts = await data.json();
+        const getPosts = await data.json(); // Array
         console.log(getPosts);
         this.posts = getPosts;
       } catch (error) {
         throw error;
       }
-    
+    },
+    async consumirComentarios() {
+      const data = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+      const getComentarios = await data.json(); // genera un array
+      this.comentarios = getComentarios;
+      console.log(this.comentarios);
     }
   },
   created() {
     this.consumirPosts();
+    this.consumirComentarios();
   }
 }
 </script>
 
-<style>
+<style scoped>
+  h3 {
+    font-size: 16px;
+  }
 
+  a {
+    text-decoration: none;
+    color: #544E4E;
+    background: bisque;
+    padding: 2px 7px;
+    border-radius: 5px;
+  }
 </style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,10 +1,52 @@
 <template>
-  <h3>Un Usuario por id</h3>
+  <!-- <p>{{ $route.params.id}}</p> -->
+  <div class="container">
+    <h1 class="text-center">Información del Usuario</h1>
+
+    <table class="table">
+
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Nick</th>
+          <th>Email</th>
+          <th>Dirección</th>
+          <th>Sitio Web</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>{{ user.name }}</td>
+          <td>{{ user.username}}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address?.street}}</td>
+          <td>{{ user.website }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    async mostrarAutor() {
+      const data = await fetch(`https://jsonplaceholder.typicode.com/users/${this.$route.params.id}`);
+      const getUser = await data.json(); // es un objeto
+      this.user = getUser;
+      console.log(this.user);
 
+    }
+  },
+  created() {
+    this.mostrarAutor();
+  }
 }
 </script>
 
